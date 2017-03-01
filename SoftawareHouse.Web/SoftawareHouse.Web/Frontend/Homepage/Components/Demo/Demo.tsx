334c811e24735fad8fda0e76d288f0ab0d557b71
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2017-03-01 21:03:01 +0100

    Create demo module with style

---
+++ SoftawareHouse.Web/SoftawareHouse.Web/Frontend/Homepage/Components/Demo/Demo.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from "react";
+
+class Demo extends React.Component<any, any> {
+
+    public render() {
+        return (
+            <div className="Demo">
+                <p>ASP.NET + React, from PoznajProgramowanie.pl</p>
+            </div>
+        );
+    }
+}
+
+export default Demo;
---
+++ SoftawareHouse.Web/SoftawareHouse.Web/Frontend/Homepage/Index.tsx
@@ -0,0 +1,6 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Demo from "./Components/Demo/Demo";
+
+ReactDOM.render(<Demo/>, document.getElementById("react-homepage-root"));
