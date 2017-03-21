af1a8495a4d648bc2348ab3c263e3b0bc6df6e6c
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-03-21 23:05:18 +0100

    Add projects list component

---
+++ SoftwareHouse/SoftawareHouse.Web/Frontend/Projects/Components/ProjectsListContainer/ProjectsListContainer.tsx
@@ -0,0 +1,41 @@
+﻿import * as React from "react";
+
+interface IProjectsListContainerState {
+    loadingData: boolean;
+    projects: any[];
+}
+
+class ProjectsListContainer extends  React.Component<any, IProjectsListContainerState>{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loadingData: true,
+            projects: []
+        };
+    }
+
+    renderProjects() {
+        const hasProjects = this.state.projects.length > 0;
+
+        if (hasProjects)
+            return <p>Projects list</p>;
+        else {
+            return <p className="text-center">No data</p>;
+        }
+    }
+
+    render() : any {
+
+
+        if (this.state.loadingData) {
+            return <p className="text-center">Loading...</p>;
+        }
+        else {
+            return this.renderProjects();
+        }
+    }
+}
+
+export default ProjectsListContainer;
