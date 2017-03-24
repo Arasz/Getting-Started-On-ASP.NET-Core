8d2bb3f700a7de2ea1ec18ee795646e47c62f31a
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-03-24 20:43:03 +0100

    Finish empty list warning component

--- SoftwareHouse/SoftawareHouse.Web/Frontend/Projects/Components/EmptyListWarning/EmptyListWarning.tsx
+++ SoftwareHouse/SoftawareHouse.Web/Frontend/Projects/Components/EmptyListWarning/EmptyListWarning.tsx
@@ -1,18 +1,18 @@
 import * as React from "react";
 import "./EmptyListWarning.scss";
-import "../../../Shared/Components/Button";
+import Button from "../../../Shared/Components/Button";
 
 class EmptyListWarning extends React.Component<any,any>{
 
     private paths = {
         createProjectUrl:"/Projects/Add",
-        createProjectLogo:""
+        createProjectLogo:"https://cdn.iconsflow.com/qwA3QsaXac57Uh3BdV3syAAV_B3i9CxkmLGGoXV4PD9c7IZZ.png"
     }
 
     public render() : any {
         return (
             <section className="EmptyListWarning"> 
-                <img className="center-block"/>
+                <img className="center-block" src={this.paths.createProjectLogo}/>
                 <section className="EmptyListWarning-textContainer">
                     <p className='text-center'>It looks like you don't have any projects created.</p>
                     <p className='text-center'>
@@ -26,3 +26,4 @@ class EmptyListWarning extends React.Component<any,any>{
     }
 
 }
+export default EmptyListWarning;
