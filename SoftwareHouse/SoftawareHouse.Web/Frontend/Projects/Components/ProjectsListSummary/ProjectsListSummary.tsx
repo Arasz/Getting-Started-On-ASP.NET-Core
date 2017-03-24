f55f6a64f207c443a93e376a50f342277a8d6154
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-03-24 22:16:57 +0100

    Add project list summary componetn

---
+++ SoftwareHouse/SoftawareHouse.Web/Frontend/Projects/Components/ProjectsListSummary/ProjectsListSummary.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import Project from '../../Models/Project';
+import Button from '../../../Shared/Components/Button';
+
+interface IProjectsListProps {
+    projects: Project[]
+}
+
+class ProjectsListSummary extends React.Component<IProjectsListProps, any> {
+
+    private paths = {
+        createProjectUrl: '/Projects/Add'
+    };
+
+    render() {
+        return (
+            <div className='row'>
+                <div className='ProjectsList-summary col-md-12'>
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <p className='ProjectList-summaryText'>Number of projects: <strong>{this.props.projects.length}</strong></p>
+                        </div>
+                        <div className='col-md-6'>
+                            <Button hasLink={true} href={this.paths.createProjectUrl} extraClassNames={['btn-success', 'pull-right']}>Start new project</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default ProjectsListSummary;
